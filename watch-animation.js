2b2e50c1c43d96ff96a3c73ad2af14ac4140da97
Monir181 <142913930+Monir181@users.noreply.github.com>
2023-09-03 10:43:52 +0600

    add watch-animation.js

---
+++ watch-animation.js
@@ -0,0 +1,92 @@
+TweenMax.from(".logo",1 ,{
+    opacity:0,
+    x:-20,
+    ease: "power4.out"
+})
+TweenMax.from(".navbar-nav",1 ,{
+    opacity:0,
+    x:-25,
+    ease: "power3.out"
+})
+TweenMax.from(".search",1 ,{
+    opacity:0,
+    x:-25,
+    delay:.25,
+    ease: "power3.out"
+})
+
+
+TweenMax.from(".line1",1 ,{
+    opacity:0,
+    y:-800,
+    delay:2,
+    ease: "power3.out"
+})
+TweenMax.from(".line2",1 ,{
+    opacity:0,
+    y:-800,
+    delay:2.5,
+    ease: "power3.out"
+})
+TweenMax.from(".img",1 ,{
+    opacity:0,
+    y:-800,
+    delay:2.9,
+    ease: "power3.out"
+})
+TweenMax.from(".main-txt",1 ,{
+    opacity:0,
+    y:20,
+    delay:1,
+    ease: "power3.out"
+})
+TweenMax.from(".btn",1 ,{
+    opacity:0,
+    y:20,
+    delay:1.3,
+    ease: "power3.out"
+})
+TweenMax.from(".year",1 ,{
+    opacity:0,
+    y:-20,
+    delay:1.4,
+    ease: "power3.out"
+})
+TweenMax.from(".icon",1.5 ,{
+    opacity:0,
+    y:50,
+    delay:3.9,
+    ease: "power3.out"
+})
+TweenMax.from(".search-m",1 ,{
+    opacity:0,
+    x:-25,
+    delay:.25,
+    ease: "power3.out"
+})
+TweenMax.from(".navbar-toggler",1.2 ,{
+    opacity:0,
+    x:-25,
+    delay:.4,
+    ease: "power3.out"
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
